12a8319da842a7a8e5e3fc808272682e8a97ed96
Miquel Adell Borràs <miquel.adell@gmail.com>
2016-01-22 16:16:13 +0100

    first commit after sucessful docker installation

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf /Users/miqueladell/code/miqueladell_v2/html
 docker rm -f miqueladellv2_wordpress_1
-docker build -f Dockerfile -t miqueladell/composed_wordpress_test .
+docker build -f Dockerfile -t miqueladell/composed_wordpress_test . 
 docker-compose -f ~/code/miqueladell_v2/docker-compose.yml up
 exit 0
 
